a1ce7c3d7a28359454352aa30b2e8672b96a1f7f
armaneousuf <armaneousuf97@gmail.com>
2026-09-16 17:02:30 +0600

    update: 2026-09-16 17:02

--- drumKit/script.js
+++ drumKit/script.js
@@ -1,22 +1,23 @@
-function playSound(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    const h1 = document.querySelector('h1');
-    h1.textContent = `The key is ${e.keyCode}`
-    if(!audio) return;
-    const keys = document.querySelectorAll('.key');
-    keys.forEach(k => {
-        if(k.classList.contains('playing')){
-            k.classList.remove('playing')
-        }
-        setTimeout(() => {
-            key.classList.remove('playing')
-        }, 500);
-    })
-    key.classList.add('playing');
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const h1 = document.querySelector("h1");
+  if (!key) {
+    h1.textContent = `${e.keyCode} is invalid`;
+    h1.style.color = "tomato";
+  } else {
+    h1.textContent = `${e.keyCode} is the valid`;
+    h1.style.color = "#80ed99";
+  }
+  if (!audio) return;
+  key.classList.add("playing");
 
-    audio.currentTime = 0
-    audio.play()
+  setTimeout(() => {
+    key.classList.remove("playing");
+  }, 500);
+
+  audio.currentTime = 0;
+  audio.play();
 }
 
-window.addEventListener('keydown', playSound)
+window.addEventListener("keydown", playSound);
