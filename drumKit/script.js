4179a722c3b4315d43d91903c12f8b9e8519cde5
armaneousuf <armaneousuf97@gmail.com>
2026-09-16 20:55:47 +0600

    feat: updated code

--- drumKit/script.js
+++ drumKit/script.js
@@ -1,12 +1,13 @@
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  const keyCode = e.keyCode || e.currentTarget.dataset.key; 
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${keyCode}"]`);
   const h1 = document.querySelector("h1");
   if (!key) {
-    h1.textContent = `${e.keyCode} is invalid`;
+    h1.textContent = `${keyCode} is invalid`;
     h1.style.color = "#f06543";
   } else {
-    h1.textContent = `${e.keyCode} is the valid`;
+    h1.textContent = `${keyCode} is the valid`;
     h1.style.color = "#80ed99";
   }
   if (!audio) return;
@@ -15,11 +16,14 @@ function playSound(e) {
   key._playingTimeout = setTimeout(() => {
     key.classList.remove('playing')
   }, 500);
-  console.log(key);
   audio.currentTime = 0;
   audio.play();
 }
 window.addEventListener("keydown", playSound);
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('click', playSound)
+})
 
 // THE BELOW CODE CAUSES SOME TRANSITION/TRANSFORM PROBLEM WHEN HAMMARED ON THE KEYBOARD
 // const keys = document.querySelectorAll('.key');
