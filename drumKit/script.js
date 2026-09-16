3ae40334ef7e863bb09d93d1e7bcf333b5a41804
armaneousuf <armaneousuf97@gmail.com>
2026-09-16 19:05:33 +0600

    fixed: fixed the issue when mashing the keys transition/transform

--- drumKit/script.js
+++ drumKit/script.js
@@ -4,20 +4,29 @@ function playSound(e) {
   const h1 = document.querySelector("h1");
   if (!key) {
     h1.textContent = `${e.keyCode} is invalid`;
-    h1.style.color = "tomato";
+    h1.style.color = "#f06543";
   } else {
     h1.textContent = `${e.keyCode} is the valid`;
     h1.style.color = "#80ed99";
   }
   if (!audio) return;
   key.classList.add("playing");
-
-  setTimeout(() => {
-    key.classList.remove("playing");
+  clearTimeout(key._playingTimeout);
+  key._playingTimeout = setTimeout(() => {
+    key.classList.remove('playing')
   }, 500);
-
+  console.log(key);
   audio.currentTime = 0;
   audio.play();
 }
-
 window.addEventListener("keydown", playSound);
+
+// THE BELOW CODE CAUSES SOME TRANSITION/TRANSFORM PROBLEM WHEN HAMMARED ON THE KEYBOARD
+// const keys = document.querySelectorAll('.key');
+// keys.forEach(k => {
+//     k.addEventListener('transitionend', (e) =>{
+//         console.log(e);
+//         if(e.propertyName !== 'transform') return;
+//         k.classList.remove('playing')
+//     })
+// })
