90449a5657201dd3e0a83ef4ddc28bf9859a2ce6
armaneousuf <armaneousuf97@gmail.com>
2026-09-17 13:23:30 +0600

    feat: added readable time

--- jsClock/script.js
+++ jsClock/script.js
@@ -1,6 +1,7 @@
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
+const displayTime = document.querySelector('h1');
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
@@ -13,6 +14,13 @@ function setDate() {
   const hours = (now.getHours() % 12) + minutes / 60;
   const hoursDeg = (hours / 12) * 360;
   hourHand.style.transform = `translateX(-50%) rotate(${hoursDeg}deg)`;
+
+  const displayHour = String(now.getHours() % 12 || 12).padStart(2, '0');
+  const displayMin = String(now.getMinutes()).padStart(2, '0');
+  const displaySeconds = String(now.getSeconds()).padStart(2, '0');
+
+
+  displayTime.textContent = `${displayHour}: ${displayMin}: ${displaySeconds}`
 }
 
 setInterval(setDate, 1000);
