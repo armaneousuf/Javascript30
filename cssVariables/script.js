2f92e9732814a4e770f0c02686845668104919a9
armaneousuf <armaneousuf97@gmail.com>
2026-09-19 11:38:26 +0600

    feat: completed html and css

---
+++ cssVariables/script.js
@@ -0,0 +1,5 @@
+const spacing = document.querySelector('#spacing')
+const blur = document.querySelector('#blur');
+const color = document.querySelector('#color');
+
+console.log(spacing, blur, color);
