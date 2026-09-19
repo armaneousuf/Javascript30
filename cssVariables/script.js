c34cfcdc965378b3d6c6e29c6cc7ef6739737fe4
armaneousuf <armaneousuf97@gmail.com>
2026-09-19 11:58:23 +0600

    feat: complete project 3

--- cssVariables/script.js
+++ cssVariables/script.js
@@ -1,5 +1,15 @@
-const spacing = document.querySelector('#spacing')
-const blur = document.querySelector('#blur');
-const color = document.querySelector('#color');
+const inputs = document.querySelectorAll('.container input');
 
-console.log(spacing, blur, color);
+console.log(inputs);
+
+function updateChanges(){
+    console.log(this);
+    console.log(this.value);
+    console.log(this.id);
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.id}`, this.value + suffix);
+}
+
+inputs.forEach(input => {
+    input.addEventListener('input', updateChanges)
+})
