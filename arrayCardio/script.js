12f5031ae2e97bd35a51f3c67add34d7e0d08fec
armaneousuf <armaneousuf97@gmail.com>
2026-09-22 11:20:39 +0600

    update: 2026-09-22 11:20

--- arrayCardio/script.js
+++ arrayCardio/script.js
@@ -95,18 +95,25 @@ const oldest = inventors.sort((a, b) => {
   const firstGuy = b.passed - b.year;
   return lastGuy > firstGuy ? -1 : 1;
 });
-console.table(oldest);
+// console.table(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-const category = document.querySelector(".mw-category");
-const links = Array.from(category.querySelectorAll("a"));
-const de = links
-          .map((link) => link.textContent)
-          .filter(streetNames => streetNames.includes('de'))
+// const category = document.querySelector(".mw-category");
+// const links = Array.from(category.querySelectorAll("a"));
+// const de = links
+//           .map((link) => link.textContent)
+//           .filter(streetNames => streetNames.includes('de'))
 // console.log(de)
+
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(', ');
+  const [bLast, bFirst] = nextOne.split(', ')
+  return aLast > bLast ? 1 : -1;
+});
+console.log(alpha)
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
