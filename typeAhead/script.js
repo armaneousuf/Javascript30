a8f66061e8eab44abd89ad362c3f9faf522b6d55
armaneousuf <armaneousuf97@gmail.com>
2026-09-24 13:03:38 +0600

    update: 2026-09-24 13:03

--- typeAhead/script.js
+++ typeAhead/script.js
@@ -5,6 +5,19 @@ const endpoint =
 // console.log(searchInput, suggestions, endpoint)
 let cities = [];
 fetch(endpoint)
-.then(blob => blob.json())
-.then(data => cities.push(...data));
+  .then((blob) => blob.json())
+  .then((data) => cities.push(...data));
 
+function findMatches(wordToMatch, cities) {
+  return cities.filter((place) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function displayMatches() {
+  
+}
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches)
